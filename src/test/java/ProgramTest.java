017e6b2f7ed0fa82b6285e43dd62da2891f88683
AdamGrzonkowski <amazing.asg@gmail.com>
2020-01-25 10:25:38 +0100

    JavaPoz21-zoo initial commit.

--- src/test/java/ProgramTest.java
+++ src/test/java/ProgramTest.java
@@ -1,14 +1,33 @@
 import Services.Animals.IAnimalService;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
 
 import static org.mockito.Mockito.*;
 
 public class ProgramTest {
+    @Rule
+    public final ExpectedSystemExit exit = ExpectedSystemExit.none();
+
     @Test
     public void ProcessCommand_OnGetCountOfAll_CallsCorrectAnimalServiceMethod(){
         IAnimalService animalServiceMock = mock(IAnimalService.class);
         Program.ProcessCommand("getCountOfAll", animalServiceMock);
 
         verify(animalServiceMock, times(1)).getNumberOfAllAnimals();
+    }
+
+    @Test
+    public void ProcessCommand_OnGetCountOfPolarBear_CallsCorrectAnimalServiceMethod(){
+        IAnimalService animalServiceMock = mock(IAnimalService.class);
+        Program.ProcessCommand("getCountOf Polar Bears", animalServiceMock);
+
+        verify(animalServiceMock, times(1)).getAnimalsCount();
+    }
+
+    @Test
+    public void ProcessCommand_Quit_ExitsProgramWithStatus0(){
+        exit.expectSystemExitWithStatus(0);
+        Program.ProcessCommand("quit", null);
     }
 }

35b5eaacb1122e4ce314af24fde33aeed7992950
AdamGrzonkowski <amazing.asg@gmail.com>
2020-01-26 14:13:39 +0100

    JavaPoz21 - main view with some basic functionality and integration with AnimalService.

--- src/main/java/Views/Main.java
+++ src/main/java/Views/Main.java
@@ -1,6 +1,11 @@
 package Views;
 
+import Services.Animals.AnimalService;
+import Services.Animals.IAnimalService;
+
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * This Main view should be used in Controller class, but we do not complicate things for now,
@@ -14,7 +19,21 @@ public class Main extends JFrame {
      */
     public JPanel jPanel;
     private JButton button1;
-    private JTextField textField1;
+    private JLabel animalCountLabel;
+    private JTextField animalCountField;
+
+    public Main(IAnimalService animalService) {
+        /**
+         * Define logic to be performed on button's click event (on the button with fieldName 'button1')
+         */
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int animalCount = animalService.getNumberOfAllAnimals();
+                animalCountField.setText(String.valueOf(animalCount));
+            }
+        });
+    }
 
     /**
      * This main method should be defined elsewhere, for example in Program.java class
@@ -25,7 +44,10 @@ public class Main extends JFrame {
     public static void main(String args[]) {
         JFrame frame = new JFrame("App"); // we create frame to contain view components of our app
 
-        frame.setContentPane(new Main().jPanel); // we set jPanel (the one from designer) to be the one used as view source for JFrame 'frame'
+        IAnimalService animalService = new AnimalService();
+        Main mainView = new Main(animalService);
+
+        frame.setContentPane(mainView.jPanel); // we set jPanel (the one from designer) to be the one used as view source for JFrame 'frame'
         frame.pack(); // we set frame to adjust its size to the elements it contains
         frame.setSize(600,600); // set width and height in pixels, for the window
         frame.setDefaultCloseOperation(EXIT_ON_CLOSE); // exit the program when X is clicked in the right-upper corner
